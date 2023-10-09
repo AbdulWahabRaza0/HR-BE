9aa8b24a176d13e564373e31b19bc9c20981e8f3
AbdulWahabRaza0 <wahab@authornate.com>
2023-10-09 14:10:28 +0500

    implemented user endpoints and protected routes

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const { connectDB } = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 const PersonalRoutes = require("./routes/personalRoute");
 const departmentRoutes = require("./routes/departmentRoute");
+const { notFound, errorHandler } = require("./middleware/error");
 app.use(express.json());
+
 connectDB();
 app.get("/", (req, res) => {
-  res.status(200).json("Hello");
+  res.status(200).json("HRS Server");
 });
+app.use("/api/user", userRoutes);
 app.use("/api/person", PersonalRoutes);
 app.use("/api/department", departmentRoutes);
-
+app.use(notFound);
+app.use(errorHandler);
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("listening to port ", port);
