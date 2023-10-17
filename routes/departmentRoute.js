a2885a4bfb1ad3b431fdeccf5f6ad1293f3b9a71
AbdulWahabRaza0 <wahab@authornate.com>
2023-10-17 16:33:33 +0500

    changed flow of schema and added admin middleware

--- routes/departmentRoute.js
+++ routes/departmentRoute.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { protect } = require("../middleware/auth.js");
+const { protect, protectAdmin } = require("../middleware/auth.js");
 const {
   addDetails,
   updateDetails,
   deleteDepartment,
   allDetails,
-  addRole2Department,
 } = require("../controller/department");
 router.get("/", protect, allDetails);
-router.post("/add", protect, addDetails);
-router.put("/role/add", protect, addRole2Department);
-router.put("/update", protect, updateDetails);
-router.put("/delete", protect, deleteDepartment);
+router.post("/add", protect, protectAdmin, addDetails);
+
+router.put("/update", protect, protectAdmin, updateDetails);
+router.put("/delete", protect, protectAdmin, deleteDepartment);
 module.exports = router;
